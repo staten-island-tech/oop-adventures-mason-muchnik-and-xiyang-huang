4b6bfb7f488708e95e44a2cf626f2e31046c6699
xiyangab <xiyangh@nycstudents.net>
2025-12-19 09:26:16 -0500

    f

---
+++ goblin.py
@@ -0,0 +1,9 @@
+class Goblin():
+    def __init__(self, hitpoints, speed, inventory):
+        self.hitpoints = hitpoints
+        self.speed = speed
+        self.inventory = inventory
+
+        hitpoints = 80
+        speed = 300
+        inventory = ["Starter Dagger"]
