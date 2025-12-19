878fe29566da8896e6a12aba5e71481aea2345be
1MasonMuch <masonm126@nycstudents.net>
2025-12-19 09:21:32 -0500

    1

--- test.py
+++ test.py
@@ -1,43 +1,24 @@
-# Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((640,640))
+
+whalen = pygame.image.load('whalen.jpg')
+
+
+
 running = True
-dt = 0
+x = 0 
+clock = pygame.time.Clock()
+while running: 
+    
+    screen.blit(whalen, (x, 30))
+    x += 1
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
+    
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
+    clock.tick(60)
 pygame.quit()
