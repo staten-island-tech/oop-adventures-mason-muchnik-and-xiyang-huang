2cd18ce0ad1af8948794ebdd81ae6a5e045151a7
xiyangab <xiyangh@nycstudents.net>
2026-01-05 09:26:08 -0500

    aaaa

--- Player.py
+++ Player.py
@@ -1,35 +1,70 @@
-class Player:
+import random
+
+class Player():
     def __init__(self, race):
         self.race = race
         self.inventory = []
 
         if race == "Human":
             self.hitpoints = 100
+            self.maxhp = 100
             self.speed = 250
             self.inventory.append("Starter Sword")
             
         elif race == "Goblin":
             self.hitpoints = 80
+            self.maxhp = 80
             self.speed = 300
             self.inventory.append("Starter Dagger")
 
         elif race == "Ogre":
             self.hitpoints = 120
+            self.maxhp = 120
             self.speed = 200
             self.inventory.append("Starter Club")
 
-#starts game
-#create gui to start game
-cc = input("Character Creation: \n1. Human \n2. Goblin\n3. Ogre\n\nWhat race do you want to be?").lower().strip()
+    def take_damage(self, enemy):
+        attack = random.choice(list(enemy.attacks.keys()))
+        damage = enemy.attacks[attack]
+        self.hitpoints -= damage
+        print(f"Skeleton used {attack} for {damage} damage!")
+        return self.hitpoints
+    
+    def attack(self, enemy):
+        for i in john.inventory:
+            if i in Weapons["name"]:
+                a = Weapons[a]
+        enemy.hitpoints -= Weapons[a]["damage"]
 
-if cc == "human":
-    test = Player("Human")
-    print(test.race)
-elif cc == "goblin":
-    test = Player("Goblin")
-    print(test.race)
-elif cc == "ogre":
-    test = Player("Ogre")
-    print(f"Your race is: {test.race}")
-else:
-    print("That is not an option.")
+class Skeleton():
+    def __init__(self):
+        self.hitpoints = 10
+        self.speed = 180
+        self.xp = 5
+        self.attacks = {
+            "Punch": 5,
+            "Bone Throw": 10
+        }
+
+    def respawn(self):
+        if self.hitpoints <= 0:
+            self.hitpoints = 10    
+
+Weapons = [
+    {"name": "Starter Sword", "damage": 15},
+    {"name": "Wooden Sword", "damage": 25},
+    {"name": "Iron Sword", "damage": 40},
+    {"name": "Starter Dagger", "damage": 10},
+    {"name": "Stone Dagger", "damage": 15},
+    {"name": "Short Sword", "damage": 25},
+    {"name": "Starter Club", "damage": 30},
+    {"name": "Spiked Club", "damage": 40},
+    {"name": "Iron Axe", "damage": 60}
+]
+
+skeleton = Skeleton()
+john = Player("Human")
+
+print(john.hitpoints)
+john.take_damage(skeleton)
+john.attack(skeleton)
