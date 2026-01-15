9a535d3bb86a42ceda9094ad33352917f159eaf9
xiyangab <xiyangh@nycstudents.net>
2026-01-14 20:40:39 -0500

    a

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class Enemy:
         self.attacks = attacks
 
     def respawn(self):
-        print(f"You have defeated the {self.name}!")
         self.hitpoints = self.maxhp
 
 
@@ -81,11 +80,13 @@ Weapons = {
     "Iron Sword": 40,
     "Starter Dagger": 10,
     "Stone Dagger": 15,
-    "Short Sword": 25,
+    "Iron Dagger": 30,
     "Starter Club": 30,
     "Spiked Club": 40,
+    "Iron Club": 55,
     "Iron Axe": 60
 }
+
 Armor = [
     {"name": "Leather Armor", "health": 10},
     {"name": "Chainmail Armor", "health": 10, "speed": 20},
@@ -134,7 +135,7 @@ class Player:
             print(f"you have {self.hitpoints} hp left")
 
     def attack(self, enemy):
-        weapon_name = self.inventory[0]
+        weapon_name = self.inventory[3]
         damage = Weapons[weapon_name]
 
         enemy.hitpoints = max(0, enemy.hitpoints - damage)
@@ -201,6 +202,8 @@ class Encounter:
                 print("ur exhausted and rest against a large rock")
                 print("suddenly a huge shadow appears from above you")
 
+            enemy.respawn()
+
             if enemy.hitpoints > 0:
                 print(f"\nA {enemy.name} appears!")
 
@@ -233,10 +236,10 @@ class Encounter:
                     elif "Apple" in player.inventory:
                         print("u have an apple")
                         use = input(f"\n1. use |2. close inventory |:").lower().strip()
-                            if use in ("1", "use"):
-                                print("you have ate an apple and gained 10 hp")
-                                player.hitpoints += 10
-                                print(f"your hp is now {player.hitpoints}")
+                        if use in ("1", "use"):
+                            print("you have ate an apple and gained 10 hp")
+                            player.hitpoints += 10
+                            print(f"your hp is now {player.hitpoints}")
                     
 
 
@@ -244,17 +247,57 @@ class Encounter:
                     print("you failed to flee slow fart!")
                     player.take_damage(enemy)
 
-    def inbetween():
-        print("phew! you survived the last battle")
-        e = random.choice(1,3)
-        #make an encounter of 1 2 or 3
-        #make it somethibg different
+    def inbetween(self):
+        print("\nphew! you survived the battle")
+        e = random.randint(1,3)
+
         if e == 1:
-            print("you have found a tree")
-            chop = input("\n1. chop |2. leave |:").lower().strip()
+            print("\nyou have found a tree")
+            chop = input("1. chop |2. leave |:").lower().strip()
             if chop in ("1", "chop"):
                 print("u have chopped the tree down. you have obtained 2 wood and 3 apples")
-                player.inventory.append("Wood", "Wood", "Apple", "Apple", "Apple")
+                player.inventory.extend(["Wood", "Wood", "Apple", "Apple", "Apple"])
+
+        elif e == 2:
+            print("\nyou have found a cave")
+            cave = input("1. enter |2. leave |:").lower().strip()
+            if cave in ("1", "enter"):
+                print("\nu have entered the cave. you find iron ore")
+                mine = input("1. mine |2. leave |:").lower().strip()
+                if mine in ("1", "mine"):
+                    print("\nu have mined the iron. u have obtained 2 pieces of iron")
+                    player.inventory.append("Iron")
+                else:
+                    print("why would u leave free iron")
+            else:
+                print("what a baby")
+
+        elif e == 3 and "Iron Sword" not in player.inventory and "Iron Dagger" not in player.inventory and "Iron Club" not in player.inventory:
+            print("\nyou have found a village and is talking to the blacksmith")
+            print("Blacksmith: if you trade me 1 piece of iron ill give u an iron weapon")
+            trade = input("1. trade |2. leave |:").lower().strip()
+            if trade in ("1", "trade") and "Iron" in player.inventory:
+                print("u are now trading with the blacksmith")
+                if player.race == "Human":
+                    print("you have obtained an Iron Sword")
+                    index = player.inventory.index("Starter Sword")
+                    player.inventory[index] = "Iron Sword"
+
+                if player.race == "Goblin":
+                    print("you have obtained an Iron Dagger")
+                    index = player.inventory.index("Starter Dagger")
+                    player.inventory[index] = "Iron Dagger"
+
+                if player.race == "Ogre":
+                    print("you have obtained an Iron Club")
+                    index = player.inventory.index("Starter Club")
+                    player.inventory[index] = "Iron Club"
+
+            else:
+                print("u dont have any iron fat scammer")
+            
+        player.hitpoints += 10
+        print("ur now kinda well rested. u have gained 10 hp.\nhopefully there isnt another battle")
 
 #starts game
 LORE = """
@@ -293,5 +336,8 @@ print(LORE)
 input("\nPress Enter to begin your journey...")
 alive = True
 while alive:
-    alive = encounter.play(player)
-    # player.show_stats()
+    encounter.play(player)
+    if player.hitpoints > 0:
+        encounter.inbetween()
+    else:
+        player.show_stats()
